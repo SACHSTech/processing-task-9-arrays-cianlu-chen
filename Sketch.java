5947c87b6450a4d874dd8a87482025dacf9e6750
cianlu-chen <kalina.chen25@ycdsbk12.ca>
2023-12-14 09:41:34 -0500

    images down

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,79 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
+  PImage imgBackground;
+  PImage imgWalkRight;
+  PImage imgWalkLeft;
+  PImage imgStandStill;
+
+  float[] circleY = new float[75];
+  int intSnowSpeed = 1;
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
   public void settings() {
-	// put your size call here
-    size(400, 400);
+    size(750, 750);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
-    background(210, 255, 173);
+    imgBackground = loadImage("snow background.jpeg");
+    imgBackground.resize(750, 750);
+
+    imgWalkRight = loadImage("walk right.png");
+    imgWalkRight.resize(91, 138);
+    
+    imgWalkLeft = loadImage("walk left.png");
+    imgWalkLeft.resize(91, 138);
+    
+    imgStandStill = loadImage("standing still.png");
+    imgStandStill.resize(58, 138);
+
+    for (int i = 0; i < circleY.length; i++) {
+      circleY[i] = random(height);
+    }
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    image(imgBackground, 0, 0);
+    
+    image(imgWalkRight, 100, 600);
+    image(imgWalkLeft, 300, 600);
+    image(imgStandStill, 500, 600);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    for (int i = 0; i < circleY.length; i++) {
+      float circleX = width * i / circleY.length;
+      ellipse(circleX, circleY[i], 20, 20);
+  
+      circleY[i] += intSnowSpeed;
+  
+      if (circleY[i] > height) {
+        circleY[i] = 0;
+      }
+    }
+
+    if(keyPressed){
+      if(keyCode == UP){
+        intSnowSpeed += 1;
+        if(intSnowSpeed == 11){
+          intSnowSpeed = 10;
+        }
+      }
+
+      if(keyCode == DOWN){
+        intSnowSpeed -= 1;
+        if(intSnowSpeed == 0){
+          intSnowSpeed = 1;
+        }
+      }
+
+      try {
+        Thread.sleep(100);
+      } 
+      
+      catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
   }
   
-  // define other methods down here.
+  
 }
