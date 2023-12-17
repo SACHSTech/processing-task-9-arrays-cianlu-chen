5d4b4a70d727eeaef6abab52678b5694cef7bd14
cianlu-chen <kalina.chen25@ycdsbk12.ca>
2023-12-17 18:09:01 -0500

    finished

--- Sketch.java
+++ Sketch.java
@@ -2,18 +2,30 @@ import processing.core.PApplet;
 import processing.core.PImage;
 
 public class Sketch extends PApplet {
+  // Variables
   PImage imgBackground;
   PImage imgWalkRight;
   PImage imgWalkLeft;
   PImage imgStandStill;
+  PImage imgHeart;
+  PImage imgColdHeart;
+  PImage imgGameOver;
 
-  float[] circleY = new float[75];
+  boolean blnLeft = false;
+  boolean blnRight = false;
+
+  float[] snowY = new float[150];
+  float[] snowX = new float[150];
+
+  boolean[] blnSnowHide = new boolean[150];
 
   int intSnowSpeed = 1;
 
   int intManX = 375;
-  int intMaxY = 600;
-	
+  int intManSpeed = 5;
+
+  int intLives = 3;
+  	
   public void settings() {
     size(750, 750);
   }
@@ -21,6 +33,9 @@ public class Sketch extends PApplet {
   public void setup() {
     imgBackground = loadImage("snow background.jpeg");
     imgBackground.resize(750, 750);
+
+    imgGameOver = loadImage("game over.jpg");
+    imgGameOver.resize(750, 750);
 
     imgWalkRight = loadImage("walk right.png");
     imgWalkRight.resize(91, 138);
@@ -31,47 +46,81 @@ public class Sketch extends PApplet {
     imgStandStill = loadImage("standing still.png");
     imgStandStill.resize(58, 138);
 
-    for (int i = 0; i < circleY.length; i++) {
-      circleY[i] = random(height);
+    imgHeart = loadImage("heart.png");
+    imgHeart.resize(50, 50);
+
+    imgColdHeart = loadImage("cold heart.png");
+    imgColdHeart.resize(50, 50);
+
+    // Sets a random number for snowX and snowY
+    for (int i = 0; i < snowY.length; i++) {
+      snowY[i] = random(-350, 401);
+    }
+
+    for (int i = 0; i < snowX.length; i++) {
+      snowX[i] = random(width);
+    }
+
+    // Sets all of blnSnowHide to false
+    for (int i = 0; i < blnSnowHide.length; i++) {
+      blnSnowHide[i] = false;
     }
   }
 
   public void draw() {
+    // Background
     image(imgBackground, 0, 0);
     
-    if(keyPressed){
-      if(key == 'a'){
-        image(imgWalkLeft, intManX, 600);
+    // Man movement
+    if(blnLeft && intManX > -25){
+      image(imgWalkLeft, intManX - 30, 600);
+      intManX -= intManSpeed;
+    }
 
-        intManX -= 5;
-      }
-
-      else if(key == 'd'){
-        image(imgWalkRight, intManX, 600);
-
-        intManX += 5;
-      }
-
-      else{
-        image(imgStandStill, intManX, 600);
-      }
+    else if(blnRight && intManX < 700){
+      image(imgWalkRight, intManX, 600);
+      intManX += intManSpeed;
     }
 
     else{
       image(imgStandStill, intManX, 600);
     }
 
-    for (int i = 0; i < circleY.length; i++) {
-      float circleX = width * i / circleY.length;
-      ellipse(circleX, circleY[i], 20, 20);
+    // Barriers
+    if(intManX > 700){
+      intManSpeed = 0;
+    }
+
+    else if(intManX < -25){
+      intManSpeed = 0;
+    }
+
+    // Drawing the snow
+    for (int i = 0; i < snowY.length; i++) {
+      ellipse(snowX[i], snowY[i], 40, 40);
   
-      circleY[i] += intSnowSpeed;
+      snowY[i] += intSnowSpeed;
   
-      if (circleY[i] > height) {
-        circleY[i] = 0;
+      if (snowY[i] > height) {
+        snowY[i] = 0;
+      }
+
+      // Collision with player head
+      if(snowX[i] > intManX && snowX[i] < intManX + 60){
+        if(snowY[i] > 600 && snowY[i] < 650){
+          intLives--;
+          snowY[i] = 0;
+        }
+      }
+
+      // Hiding the snowflakes
+      if(blnSnowHide[i]){
+        snowY[i] = 0;
+        blnSnowHide[i] = false;
       }
     }
 
+    // Snow speed
     if(keyPressed){
       if(keyCode == UP){
         intSnowSpeed += 1;
@@ -86,16 +135,70 @@ public class Sketch extends PApplet {
           intSnowSpeed = 1;
         }
       }
+    }
 
-      try {
-        Thread.sleep(100);
-      } 
-      
-      catch (InterruptedException e) {
-        e.printStackTrace();
+    // Lives
+    image(imgHeart, 10, 10);
+    image(imgHeart, 70, 10);
+    image(imgHeart, 130, 10);
+
+    if(intLives < 3){
+      image(imgColdHeart, 130, 10);
+    }
+
+    if(intLives < 2){
+      image(imgColdHeart, 70, 10);
+    }
+
+    // Game over
+    if(intLives < 1){
+      image(imgColdHeart, 10, 10);
+      intSnowSpeed = 0;
+      intManSpeed = 0;
+      delay(1000);
+      background(0);
+      image(imgGameOver, -15, 0);
+    }
+  }
+
+  // True or false for man movement
+  public void keyPressed(){
+    if(key == 'a'){
+      blnLeft = true;
+    }
+
+    if(key == 'd'){
+      blnRight = true;
+    }
+  }
+
+  public void keyReleased(){
+    if(key == 'a'){
+      blnLeft = false;
+    }
+
+    if(key == 'd'){
+      blnRight = false;
+    }
+  }
+
+  // If user clicks a snowflake, it 'disappears'
+  public void mouseClicked(){
+    float mouseSnowX[] = new float[150];
+    float mouseSnowY[] = new float[150];
+
+    for(int i = 0; i < mouseSnowX.length; i++){
+      mouseSnowX[i] = snowX[i];
+    }
+
+    for(int i = 0; i < mouseSnowY.length; i++){
+      mouseSnowY[i] = snowY[i];
+    }
+
+    for(int i = 0; i < mouseSnowX.length; i++){
+      if(dist(mouseX, mouseY, mouseSnowX[i], mouseSnowY[i]) < 30){
+        blnSnowHide[i] = true;
       }
     }
   }
-  
-  
 }
